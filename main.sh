2904da3f1fd3d5e3202fecb425fd2c5631a95593
Xstep Ahead シ <abhiedthea@gmail.com>
2023-02-06 22:07:07 +0700

    Tambah Depencies
    Debian 10 butuh itu buat install
     add-apt-repository ppa:vbernat/haproxy-2.7 -y

--- main.sh
+++ main.sh
@@ -89,6 +89,7 @@ function base_package() {
     print_install "Memasang paket yang dibutuhkan"
     sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
     sysctl -w net.ipv6.conf.default.disable_ipv6=1  >/dev/null 2>&1
+    sudo apt install software-properties-common -y
     sudo add-apt-repository ppa:vbernat/haproxy-2.7 -y
     sudo apt update && apt upgrade -y
     # linux-tools-common util-linux gnupg gnupg2 gnupg1  \
